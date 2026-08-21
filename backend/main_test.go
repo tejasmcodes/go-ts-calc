400c27fde5bdd080dbbb0ef6816c4013c7b5e963
Tejas M <tejasmcodes@gmail.com>
2026-08-21 22:13:58 +0530

    add success and error tests for calculator API

--- backend/main_test.go
+++ backend/main_test.go
@@ -1,50 +1,154 @@
 package main
 
 import (
+	"fmt"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
-
-func TestMathHandler(t *testing.T) {
-	// create a mock http request
-	req := httptest.NewRequest(http.MethodGet, "/calculate?a=1&b=2&operation=add", nil)
-
-	// create a response recorder to capture the response
-	rec := httptest.NewRecorder()
-
-	// call the math handler directly
-	mathHandler(rec, req)
-
-	if rec.Code != http.StatusOK {
-		t.Errorf("Expected status %d, got %d", http.StatusOK, rec.Code)
+// def func to test the success
+func TestMathHandlerSuccess(t *testing.T){
+	tests := [] struct {
+	name string
+	a string
+	b string
+	operation string
+	expected float64
+	}{
+		{
+			name:	"addition",
+			a:		"10",
+			b:		"5",
+			operation:"add",
+			expected: 15,
+	},
+	{
+			name:	"subtraction",
+			a:		"10",
+			b:		"5",
+			operation:"sub",
+			expected: 5,
+	},
+	{
+			name:	"multiplication",
+			a:		"10",
+			b:		"5",
+			operation:"mul",
+			expected: 50,
+	},
+	{
+			name:	"division",
+			a:		"10",
+			b:		"5",
+			operation:"div",
+			expected: 2,
+	},
 	}
 
-	if rec.Header().Get("Content-Type") != "application/json" {
-		t.Errorf("Expected JSON content type")
+	for _, test := range tests {
+	t.Run(test.name, func(t *testing.T){
+		url := fmt.Sprintf(
+			"/calculate?a=%s&b=%s&operation=%s",
+			test.a,
+			test.b,
+			test.operation,
+		)
+		req := httptest.NewRequest(http.MethodGet, url, nil)
+		rec := httptest.NewRecorder()
+
+		mathHandler(rec, req)
+
+		if rec.Code != http.StatusOK {
+			t.Fatalf(
+				"Expected status %d, got %d", 
+				http.StatusOK,
+				rec.Code,
+
+			)
+		}
+
+		var response MathResponse
+		err := json.NewDecoder(rec.Body).Decode(&response)
+		if err != nil {
+			t.Fatalf("Failed to decode the response: %v",err)
+		}
+
+		if response.Result != test.expected {
+			t.Errorf("Expected %v, got %v",test.expected, response.Result)
+		}
+	})
+}
+
+}
+
+// def func to test the failures
+func TestMathHandlerErrors(t *testing.T){
+	tests := []struct {
+		name string
+		a 	string
+		b	string
+		operation string
+		expectedError string
+	}{
+		{
+			name:	"invalid number",
+			a:		"Hello",
+			b:		"5",
+			operation: "add",
+			expectedError: "Please provide a valid number for a and b",
+		},
+		{
+			name:	"invalid operation",
+			a:		"10",
+			b:	    "5",
+			operation: "power",
+			expectedError: "Unknown operation. Use 'add', 'sub', 'mul', 'div'",
+		},
+		{
+			name:	"division by zero",
+			a:		"10",
+			b:		"0",
+			operation: "div",
+			expectedError: "Cannot divide the number by Zero",
+		},
 	}
 
-	var response MathResponse
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T){
+			url := fmt.Sprintf("/calculate?a=%s&b=%s&operation=%s",
+		    					test.a,
+								test.b,
+								test.operation)
 
-	err := json.NewDecoder(rec.Body).Decode(&response)
-	if err != nil {
-		t.Fatalf("Failed to decoded response: %v", err)
-	}
+			req := httptest.NewRequest(http.MethodGet,url, nil)
+			rec := httptest.NewRecorder()
 
-	if response.A != 1 {
-		t.Errorf("expected A=1, got %v", response.A)
-	}
+			mathHandler(rec, req)
 
-	if response.B != 2 {
-		t.Errorf("expected B=2, got %v", response.B)
-	}
+			if rec.Code != http.StatusBadRequest {
+				t.Fatalf(
+					"Expected %d, got %d",
+					http.StatusBadRequest, 
+					rec.Code,
+				)
+			}
 
-	if response.Operation != "add" {
-		t.Errorf("Expected operation = add, got %v", response.Operation)
-	}
+			var response ErrorResponse
+			err := json.NewDecoder(rec.Body).Decode(&response)
+			if err != nil {
+				t.Fatalf("Failed to decode the response: %v",err)
+			}
 
-	if response.Result != 3 {
-		t.Errorf("Expected result=3, got %v", response.Result)
+			if response.Error != test.expectedError {
+				t.Errorf(
+					"Expected %v, got %v",
+					test.expectedError,
+					response.Error,
+				)
+			}
+
+
+		})
 	}
 }
